cff4e35169869a3a5c283f9da9c512c3d5a426b4
kx0101 <liakos.koulaxis@yahoo.com>
2024-09-07 12:34:12 +0300

    filtering pagination and sorting for get movies

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -6,10 +6,12 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
 
 	"github.com/julienschmidt/httprouter"
+	"kx0101.greenlight/internal/validator"
 )
 
 type envelope map[string]any
@@ -101,4 +103,40 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, destina
 	}
 
 	return nil
+}
+
+func (app *application) readString(qs url.Values, key string, defaultValue string) string {
+	s := qs.Get(key)
+
+	if s == "" {
+		return defaultValue
+	}
+
+	return s
+}
+
+func (app *application) readCSV(qs url.Values, key string, defaultValues []string) []string {
+	s := qs.Get(key)
+
+	if s == "" {
+		return defaultValues
+	}
+
+	return strings.Split(s, ",")
+}
+
+func (app *application) readInt(qs url.Values, key string, defaultValue int, v *validator.Validator) int {
+	s := qs.Get(key)
+
+	if s == "" {
+		return defaultValue
+	}
+
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		v.AddError(key, "must be an integer value")
+		return defaultValue
+	}
+
+	return i
 }
